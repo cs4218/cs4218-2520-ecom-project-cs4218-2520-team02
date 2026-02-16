e449b870770f36e4475b30a54fc53f885758d287
ToxOptimism <toxicoptimismben@gmail.com>
2026-02-17 01:21:45 +0800

    Test: add unit test for footer

---
+++ client/src/components/Footer.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "../components/Footer";
+import "@testing-library/jest-dom/extend-expect";
+
+// Mocks
+jest.mock("../components/Layout", () => ({
+  __esModule: true,
+  default: ({ children, title }) => (
+    <div data-testid="layout" title={title}>
+      {children}
+    </div>
+  ),
+}));
+
+const renderFooter = () =>
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
+
+describe("Footer Component Unit Tests", () => {
+
+  it("should render footer page with all elements", () => {
+    
+    // Arrange & Act
+    renderFooter();
+
+    const copyrightText = screen.getByText(/All Rights Reserved © TestingComp/)
+    const aboutLink = screen.getByText("About");
+    const contactLink = screen.getByText("Contact");
+    const privacyLink = screen.getByText("Privacy Policy");
+
+    // Assert
+    expect(copyrightText).toBeInTheDocument();
+
+    expect(aboutLink).toBeInTheDocument();
+    expect(aboutLink.closest("a")).toHaveAttribute("href", "/about");
+
+    expect(contactLink).toBeInTheDocument();
+    expect(contactLink.closest("a")).toHaveAttribute("href", "/contact");
+    
+    expect(privacyLink).toBeInTheDocument();
+    expect(privacyLink.closest("a")).toHaveAttribute("href", "/policy");
+  
+  });
+});
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,14 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/*.test.js",
+    "<rootDir>/client/src/components/*.test.js"
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/**"],
+  collectCoverageFrom: ["client/src/pages/**", "client/src/components/**"],
   coverageThreshold: {
     global: {
       lines: 0,
