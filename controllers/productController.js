d62eb8fa5fa4d4a5928656a9f7b545bdbfe0c757
Jovin Ang <36396502+Jovin-Ang@users.noreply.github.com>
2026-02-18 03:33:00 +0800

    fix(controller): correct typo in error message for single product retrieval

--- controllers/productController.js
+++ controllers/productController.js
@@ -108,7 +108,7 @@ export const getSingleProductController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
-      message: "Eror while getitng single product",
+      message: "Error while getting single product",
       error,
     });
   }
