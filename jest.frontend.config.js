78439810f96cbf8a0371c82e786851cb8b06a69d
ToxOptimism <toxicoptimismben@gmail.com>
2026-02-19 00:56:48 +0800

    Test: add test cases for useCategory hook

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -22,12 +22,13 @@ export default {
   // only run these tests
   testMatch: [
     "<rootDir>/client/src/pages/*.test.js",
-    "<rootDir>/client/src/components/*.test.js"
+    "<rootDir>/client/src/components/*.test.js",
+    "<rootDir>/client/src/hooks/*.test.js"
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/**", "client/src/components/**"],
+  collectCoverageFrom: ["client/src/pages/**", "client/src/components/**", "client/src/hooks/**"],
   coverageThreshold: {
     global: {
       lines: 0,
