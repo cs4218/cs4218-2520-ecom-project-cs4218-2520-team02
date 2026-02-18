d011d62d7b2024d023a8394bd5be64f87a3a10df
Ryuse <Leemuel01234@gmail.com>
2026-02-19 01:40:10 +0800

    Fix logic for when no filter applied

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -85,8 +85,9 @@ const HomePage = () => {
     }
     setChecked(all);
   };
+  
   useEffect(() => {
-    if (!checked.length || !radio.length) getAllProducts();
+    if (!checked.length && !radio.length) getAllProducts();
   }, [checked.length, radio.length]);
 
   useEffect(() => {
