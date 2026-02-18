b1401808a46a23207c9596a7344a496bb6c14782
GavinSin <51268353+GavinSin@users.noreply.github.com>
2026-02-18 18:45:03 +0800

    Update jest config to include authHelper

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -9,12 +9,19 @@ export default {
   testMatch: [
     "<rootDir>/**/__tests__/**/*.test.js",
     "<rootDir>/controllers/*.test.js",
+    "<rootDir>/helpers/*.test.js",
     "<rootDir>/models/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**", "middlewares/**", "models/**", "routes/**"],
+  collectCoverageFrom: [
+    "controllers/**",
+    "middlewares/**",
+    "models/**",
+    "routes/**",
+    "helpers/**",
+  ],
   coverageThreshold: {
     global: {
       lines: 0,
