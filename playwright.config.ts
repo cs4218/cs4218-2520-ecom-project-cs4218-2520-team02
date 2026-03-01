4074ff9568d12cb3b702c48d34ab05ee913646bd
Ryuse <Leemuel01234@gmail.com>
2026-03-01 19:18:55 +0800

    Add timeout on config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './__tests__/e2e/flows',
-  testMatch: [
-    "**/*.spec.ts"
-  ],
+  testMatch: ['**/*.spec.ts'],
   fullyParallel: true,
   workers: 1,
   reporter: 'html',
@@ -20,43 +18,31 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
-
-  /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
-
   webServer: [
     {
-      command: "npm run client",
+      command: 'npm run client',
       port: 3000,
       reuseExistingServer: true,
+      timeout: 120_000,
       env: {
-        PORT: "3000"
-      }
+        PORT: '3000',
+      },
     },
     {
-      command: "npm run server",
+      command: 'npm run server',
       port: 6060,
       reuseExistingServer: true,
+      timeout: 120_000,
       env: {
-        DEV_MODE: "development",
-        PORT: "6060",
-        //This string URI doesn't work at all. It's just so the config can compile.
-        MONGO_URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/e2e_test" 
+        DEV_MODE: 'development',
+        PORT: '6060',
+         //This string URI doesn't work at all. It's just so the config can compile.
+        MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/e2e_test',
       },
     },
   ],
